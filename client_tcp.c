40b4880c3674b167eddd949fbea9f1b089a990c9
geoffrey.durand <geoffreydu2727@gmail.com>
2025-12-24 10:44:21 +0100

    implementation fonctionnelle tcp mono-client

--- client_tcp.c
+++ client_tcp.c
@@ -1,23 +1,77 @@
 #include "common.h"
 
-int main() {
-    // declaration des variables
+int main(int argc, char *argv[]) {
     int sockfd;
     struct sockaddr_in serv_addr;
+    MastermindReq req;
+    MastermindRep rep;
 
-    printf("demarrage du client mastermind...\n");
+    // verification des arguments 
+    char *server_ip = "127.0.0.1";
+    if (argc > 1) {
+        server_ip = argv[1];
+    }
 
-    // etape 1 : creation de la socket tcp
-    // a faire
+    // creation socket tcp
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("erreur ouverture socket");
+        exit(1);
+    }
 
-    // etape 2 : configuration de l'adresse du serveur a contacter
-    // a faire
+    // configuration de l'adresse du serveur
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+        perror("adresse invalide");
+        exit(1);
+    }
 
-    // etape 3 : connexion au serveur (connect)
-    // a faire
+    // connexion
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("erreur connexion");
+        exit(1);
+    }
+    printf("connecte au serveur %s sur le port %d\n", server_ip, PORT);
+    printf("entrez 4 chiffres separes par des espaces (ex: 1 2 3 4)\n");
 
-    // etape 4 : boucle de jeu (envoi proposition, reception reponse)
-    // a faire
+    // boucle de jeu
+    while(1) {
+        printf("votre proposition > ");
+        int r = scanf("%d %d %d %d", &req.combinaison[0], &req.combinaison[1], &req.combinaison[2], &req.combinaison[3]);
+        
+        if (r != 4) {
+            printf("format invalide. reessayez.\n");
+            // vider le buffer clavier
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
 
+        // envoi de la proposition
+        if (write(sockfd, &req, sizeof(req)) < 0) {
+            perror("erreur ecriture");
+            break;
+        }
+
+        // lecture de la reponse
+        if (read(sockfd, &rep, sizeof(rep)) < 0) {
+            perror("erreur lecture");
+            break;
+        }
+
+        // affichage resultat
+        printf("resultat: %d bien places, %d mal places. message: %s\n", 
+               rep.bien_places, rep.mal_places, rep.message);
+
+        // verification fin de partie
+        if (rep.code_etat == 1) {
+            printf("bravo, vous avez trouve !\n");
+            break;
+        }
+    }
+
+    close(sockfd);
     return 0;
 }
